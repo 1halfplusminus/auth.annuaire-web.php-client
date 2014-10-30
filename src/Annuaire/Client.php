ddd79a82ea53ed1ff00613e7439344ddc8caaf8e
root <root@host.sion.nc>
2014-10-31 10:43:12 +1100

    init project

---
+++ src/Annuaire/Client.php
@@ -0,0 +1,19 @@
+<?php
+class Client{
+    
+    public function __construct($client_id,$client_secret){
+        
+    }
+    public function usePasswordGrant($username,$password)
+    {
+        
+    }
+    public function useRefreshTokenGrant()
+    {
+        
+    }
+    public function checkToken()
+    {
+        
+    }
+}
